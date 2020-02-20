84be5c135baf9e6a1fd810c74fcbe0f1c61a27d4
Peter Sherk <psherk95@gmail.com>
2020-02-20 13:13:26 -0500

    Converted website to Vue

---
+++ precache-manifest.0ef37bdbc74950ebf9b3d0db5108082c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d04f692523bb75f12e",
+    "url": "/css/about.cf2ecbc6.css"
+  },
+  {
+    "revision": "aa5efce2c0b01d86b397",
+    "url": "/css/app.ba938640.css"
+  },
+  {
+    "revision": "5592468e638725c46cc9",
+    "url": "/css/chunk-vendors.d9bcc5e6.css"
+  },
+  {
+    "revision": "27314327dda623c352c0266b74bb1901",
+    "url": "/docs/resume_peter_sherk.pdf"
+  },
+  {
+    "revision": "abc7d03f0fbc81ca8c5a221957850718",
+    "url": "/img/addProject.gif"
+  },
+  {
+    "revision": "d0bccc4c840ae7dc1a986cab262e85fe",
+    "url": "/img/addRole.gif"
+  },
+  {
+    "revision": "2a94a0630546cad9442576795d94add4",
+    "url": "/img/addSubProj.gif"
+  },
+  {
+    "revision": "f6b1a678a1eec50529d18b21fbeaeec1",
+    "url": "/img/deleteAndRenameProj.gif"
+  },
+  {
+    "revision": "3b6bf0b44816203cca40ec18155b003c",
+    "url": "/img/deleteSubProj.gif"
+  },
+  {
+    "revision": "9cc18a7b696c625573be115674479b11",
+    "url": "/img/editAndDeleteRole.gif"
+  },
+  {
+    "revision": "cc0c8fca175a5ae8aa5593575a88005f",
+    "url": "/img/editGates.gif"
+  },
+  {
+    "revision": "806b83a5985f1ca4d24b2acf7b68fc5b",
+    "url": "/img/email.gif"
+  },
+  {
+    "revision": "1a5687daee688239279a50a848b5ddad",
+    "url": "/img/fileOptions.gif"
+  },
+  {
+    "revision": "845eeb5fb5588f5772748beccf7c2810",
+    "url": "/img/moose_404.845eeb5f.jpeg"
+  },
+  {
+    "revision": "845eeb5fb5588f5772748beccf7c2810",
+    "url": "/img/moose_404.jpeg"
+  },
+  {
+    "revision": "be6216841946f5df0ef2831d19ef1d5d",
+    "url": "/img/old.gif"
+  },
+  {
+    "revision": "dd85e82c949b4f3bcca1a9b57741e929",
+    "url": "/img/positano.dd85e82c.jpeg"
+  },
+  {
+    "revision": "dd85e82c949b4f3bcca1a9b57741e929",
+    "url": "/img/positano.jpeg"
+  },
+  {
+    "revision": "f95816a4da7dedb858e5b54f50648e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52d04f692523bb75f12e",
+    "url": "/js/about.a5f7417a.js"
+  },
+  {
+    "revision": "aa5efce2c0b01d86b397",
+    "url": "/js/app.39b92eaf.js"
+  },
+  {
+    "revision": "5592468e638725c46cc9",
+    "url": "/js/chunk-vendors.52247e01.js"
+  },
+  {
+    "revision": "caca80d6278ab4dfa0c25242575f2ec6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
